276277167907f58ee7201c8234326677057cbca6
Grayton Savickas <gsavickaspro@gmail.com>
2021-06-06 16:07:39 -0500

    adds assignment 2.3

--- week2/enhanced-profile-app/karma.conf.js
+++ week2/enhanced-profile-app/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/appTest'),
+      dir: require('path').join(__dirname, './coverage/enhanced-profile-app'),
       subdir: '.',
       reporters: [
         { type: 'html' },
