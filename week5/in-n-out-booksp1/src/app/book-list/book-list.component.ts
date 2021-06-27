4827cf126f74158e9c31f9064c70fc526b289f1b
gsavickas <gssavickas@gmail.com>
2021-06-27 17:54:09 -0500

    adds assignment 5.3

--- week5/in-n-out-booksp1/src/app/book-list/book-list.component.ts
+++ week5/in-n-out-booksp1/src/app/book-list/book-list.component.ts
@@ -4,19 +4,33 @@
  * Date: 27 June 2021
  * Description: App component
  */
+ import { Component, OnInit } from '@angular/core';
+ import { BooksService } from '../books.service';
+ import { IBook } from '../book.interface';
+ import { Observable } from 'rxjs';
 
-import { Component, OnInit } from '@angular/core';
 
-@Component({
-  selector: 'app-book-list',
-  templateUrl: './book-list.component.html',
-  styleUrls: ['./book-list.component.scss']
-})
-export class BookListComponent implements OnInit {
+ @Component({
+   selector: 'app-book-list',
+   templateUrl: './book-list.component.html',
+   styleUrls: ['./book-list.component.css']
+ })
+ export class BookListComponent implements OnInit {
 
-  constructor() { }
+   books: Observable<IBook[]>;
+   header: Array<string> = ['isbn', 'title', 'numOfPages', 'authors'];
+   book!: IBook;
 
-  ngOnInit(): void {
-  }
+   constructor(private booksService: BooksService) {
+     this.books=this.booksService.getBooks();
+   }
 
-}
+   ngOnInit(): void {
+   }
+
+   showBookDetails(isbn:string){
+     this.book = this.booksService.getBook(isbn);
+     console.log(this.book);
+   }
+
+ }
