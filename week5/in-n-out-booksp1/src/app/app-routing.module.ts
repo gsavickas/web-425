9ce8cb0f1cf74b0d252bdfd296167768fb2b8b90
gsavickas <gssavickas@gmail.com>
2021-06-26 23:50:52 -0500

    add assignment 5.2

--- week5/in-n-out-booksp1/src/app/app-routing.module.ts
+++ week5/in-n-out-booksp1/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: BookListComponent},
+  {path: '/book-list', component: BookListComponent},
+  {path: '/contact', component: ContactComponent},
+  {path: '/about', component: AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- week5/in-n-out-booksp1/src/app/app.module.ts
+++ week5/in-n-out-booksp1/src/app/app.module.ts
@@ -4,10 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    BookListComponent,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+    MatListModule,
+    FlexLayoutModule,
+    MatIconModule
   ],
   imports: [
     BrowserModule,
