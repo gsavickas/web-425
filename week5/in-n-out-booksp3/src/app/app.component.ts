27953f450e7b83ab54c547f83e666a7ddf6afb9b
gsavickas <gssavickas@gmail.com>
2021-06-27 18:26:54 -0500

    adds assignment 5.3

---
+++ week5/in-n-out-booksp3/src/app/app.component.ts
@@ -0,0 +1,17 @@
+/**
+ * Title: app.component.ts
+ * Author: Grayton Savickas
+ * Date: 27 June 2021
+ * Description: App component
+ */
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  assignment = 'Assignment 5.2 - Navigation and Layout';
+}
---
+++ week5/in-n-out-booksp3/src/app/book-list/book-list.component.spec.ts
@@ -0,0 +1,32 @@
+/**
+ * Title: app.component.ts
+ * Author: Grayton Savickas
+ * Date: 27 June 2021
+ * Description: App component
+ */
+
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BookListComponent } from './book-list.component';
+
+describe('BookListComponent', () => {
+  let component: BookListComponent;
+  let fixture: ComponentFixture<BookListComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BookListComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BookListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
