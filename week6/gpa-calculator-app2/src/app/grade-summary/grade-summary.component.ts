72fad594efcbbd81b040b8bab612bb9ce700f165
Grayton Savickas <gsavickaspro@gmail.com>
2021-07-04 23:09:55 -0500

    adds assignment 6.4

---
+++ week6/gpa-calculator-app2/src/app/grade-summary/grade-summary.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grade-summary',
+  templateUrl: './grade-summary.component.html',
+  styleUrls: ['./grade-summary.component.css']
+})
+export class GradeSummaryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
